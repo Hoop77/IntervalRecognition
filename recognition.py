90bc089449265251adae72a6ae447b1dce5e8de7
Hoop77 <p.badenhoop@gmx.de>
2018-01-31 15:07:18 +0100

    intervals

--- recognition.py
+++ recognition.py
@@ -13,17 +13,17 @@ CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
 FRAME_SIZE = CHUNK * CHANNELS
-RECORDED_FRAMES = 64
+RECORDED_FRAMES = 32
 RECORDED_SIZE = RECORDED_FRAMES * FRAME_SIZE
 AMPLITUDE_THRESHOLD = 2.0
 WINDOW_SIZE = 10
 SLEEP_TIME = 0.01
-UPDATE_UI = False
+UPDATE_UI = True 
 INTERVAL_TOLERANCE = 100. # in cent (100 cent == minor second)
 FREQUENCIES = int(RECORDED_SIZE / 2)
 
-def freq_of_tone_idx(tone_idx):
-    return 440 * (np.power(np.power(2, 1./12.), tone_idx-49))
+def freq_of_note_idx(note_idx):
+    return 440 * (np.power(np.power(2, 1./12.), note_idx-49))
 
 def interval_to_cent(freq1, freq2):
     high_freq = max(freq1, freq2)
@@ -32,7 +32,7 @@ def interval_to_cent(freq1, freq2):
         return float('inf')
     return 1200. * np.log2(high_freq / low_freq)
 
-tone_labels = [
+note_labels = [
     "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
     "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
     "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
@@ -43,13 +43,13 @@ tone_labels = [
     "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
     "C8"
 ]
-tones = [freq_of_tone_idx(tone) for tone in range(-8, 88+1)]
-label_of_tone = {tones[i]: tone_labels[i] for i in range(len(tones))}
+notes = [freq_of_note_idx(note) for note in range(-8, 88+1)]
+label_of_note = {notes[i]: note_labels[i] for i in range(len(notes))}
 
-tone_series_labels = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-tone_series = [tones[i::12] for i in range(12)]
+note_series_labels = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+note_series = [notes[i::12] for i in range(12)]
 
-series_idx_of_tone = {tone: (i % 12) for i, tone in enumerate(tones)}
+series_idx_of_note = {note: (i % 12) for i, note in enumerate(notes)}
 
 intervals = [
     "prime",
@@ -67,29 +67,35 @@ intervals = [
     "octave"
 ]
 
-def print_tone_series():
-    for idx, series in enumerate(tone_series):
-        for i, tone in enumerate(series):
-            print("{}{}: {:.2f}Hz".format(tone_series_labels[idx], i, tone))
+def note_idx_to_interval(note_series_idx0, note_series_idx1):
+    if note_series_idx0 <= note_series_idx1:
+        return intervals[note_series_idx1 - note_series_idx0]
+    else:
+        return intervals[12 - (note_series_idx0 - note_series_idx1)]
+
+def print_note_series():
+    for idx, series in enumerate(note_series):
+        for i, note in enumerate(series):
+            print("{}{}: {:.2f}Hz".format(note_series_labels[idx], i, note))
 
 def comp(freq, l, r):
     m = l + int((r - l) / 2)
     if l == m:
-        if np.abs(freq - tones[l]) <= np.abs(freq - tones[r]):
-            return tones[l]
+        if np.abs(freq - notes[l]) <= np.abs(freq - notes[r]):
+            return notes[l]
         else: 
-            return tones[r]
-    elif freq <= tones[l]:
-        return tones[l]
-    elif freq > tones[l] and freq <= tones[m]:
+            return notes[r]
+    elif freq <= notes[l]:
+        return notes[l]
+    elif freq > notes[l] and freq <= notes[m]:
         return comp(freq, l, m)
-    elif freq > tones[m] and freq < tones[r]:
+    elif freq > notes[m] and freq < notes[r]:
         return comp(freq, m, r)
     else:
-        return tones[r]
+        return notes[r]
 
-def get_nearest_tone(freq):
-    return comp(freq, 0, len(tones) - 1)
+def get_nearest_note(freq):
+    return comp(freq, 0, len(notes) - 1)
 
 def get_spectrum(recorded_data):
     spectrum = np.fft.fft(recorded_data)[:FREQUENCIES]
@@ -99,34 +105,34 @@ def get_spectrum(recorded_data):
 def make_frequencies():
     return np.fft.fftfreq(RECORDED_SIZE, d=1.0/(RATE * CHANNELS))[:FREQUENCIES]
 
-def make_nearest_tone_of_frequency_dict(frequencies):
+def make_nearest_note_of_frequency_dict(frequencies):
     result = {}
     for freq in frequencies:
-        nearest_tone = get_nearest_tone(freq)
-        if interval_to_cent(nearest_tone, freq) > INTERVAL_TOLERANCE:
-            nearest_tone = None
-        result[freq] = nearest_tone
+        nearest_note = get_nearest_note(freq)
+        if interval_to_cent(nearest_note, freq) > INTERVAL_TOLERANCE:
+            nearest_note = None
+        result[freq] = nearest_note
     return result
 
 frequencies = make_frequencies()
 
 eq_points = {
     0: 0,
-    tones[0]: 1,
+    100: 1
 }
 eq_filter = eq(frequencies, eq_points)
 
-nearest_tone_of_frequency = make_nearest_tone_of_frequency_dict(frequencies)
+nearest_note_of_frequency = make_nearest_note_of_frequency_dict(frequencies)
 
-def get_tone_series_idx_from_spectrum(frequencies, spectrum):
-    # Add the amplitude of each tone series together and return the series with the maximum sum.
-    # This might be the one with the most impact on a certain tone.
+def get_note_series_idx_from_spectrum(frequencies, spectrum):
+    # Add the amplitude of each note series together and return the series with the maximum sum.
+    # This might be the one with the most impact on a certain note.
     series_sum = {series_idx: 0 for series_idx in range(12)}
     for i, freq in enumerate(frequencies):
-        nearest_tone = nearest_tone_of_frequency[freq]
-        if nearest_tone is None:
+        nearest_note = nearest_note_of_frequency[freq]
+        if nearest_note is None:
             continue
-        nearest_series_idx = series_idx_of_tone[nearest_tone]
+        nearest_series_idx = series_idx_of_note[nearest_note]
         series_sum[nearest_series_idx] += spectrum[i]
     return max(series_sum, key=series_sum.get)
 
@@ -135,10 +141,10 @@ class Window:
         self.size = size
         self.data = [None] * size
 
-    def put(self, tone):
+    def put(self, note):
         # rotate left
         self.data[:-1] = self.data[1:]
-        self.data[-1] = tone
+        self.data[-1] = note
 
     def get(self):
         weights = {}
@@ -157,11 +163,11 @@ def threshold_filter(spectrum, threshold):
         if y < threshold:
             spectrum[i] = 0.
 
-def plot_tone_series(series_label, color=(1, 0, 1, 0.5)):
-    tone_series_idx = tone_series_labels.index(series_label)
+def plot_note_series(series_label, color=(1, 0, 1, 0.5)):
+    note_series_idx = note_series_labels.index(series_label)
     lines = []
-    for tone in tone_series[tone_series_idx]:
-        line = plt.axvline(x=tone, color=color, linewidth=1.5)
+    for note in note_series[note_series_idx]:
+        line = plt.axvline(x=note, color=color, linewidth=1.5)
         lines.append(line)
     return lines
 
@@ -188,7 +194,7 @@ callback.recorded_data_mutex = Lock()
 window_function = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, RECORDED_SIZE, False)))
 
 window = Window()
-num_tones_recognized = 0
+num_notes_recognized = 0
 last_series_idx = None
 
 audio = pyaudio.PyAudio()
@@ -207,7 +213,7 @@ original_spectrum_graph, = ax.plot(frequencies, y, color=(0, 0, 0, 0.3))
 modified_spectrum_graph, = ax.plot(frequencies, y, color=(0, 0, 1, 0.5))
 threshold_graph, = ax.plot(frequencies, threshold, color=(1, 0, 0, 1))
 eq_graph, = ax.plot(frequencies, eq_filter, color=(0, 1, 0, 1))
-glines = plot_tone_series("G")
+glines = plot_note_series("G")
 
 # start Recording
 stream = audio.open(format=PYAUDIO_FORMAT, 
@@ -235,21 +241,25 @@ while stream.is_active():
         spectrum *= eq_filter
         max_amplitude = max(spectrum)
 
-        series_idx = get_tone_series_idx_from_spectrum(frequencies, spectrum)
+        series_idx = get_note_series_idx_from_spectrum(frequencies, spectrum)
 
         if max_amplitude < AMPLITUDE_THRESHOLD:
             window.put(None)
         else:
             window.put(series_idx)
-            #print("current tone: {}".format(tone_series_labels[series_idx]))
+            #print("current note: {}".format(note_series_labels[series_idx]))
         
         curr_series_idx = window.get()
         if curr_series_idx is not last_series_idx:
             if curr_series_idx is None:
                 print("Silence")
             else:
-                print("#{} Current tone: {}".format(num_tones_recognized, tone_series_labels[curr_series_idx]))
-                num_tones_recognized += 1
+                if last_series_idx is not None:
+                    print("#{} Current note: {}, interval: {}".format(num_notes_recognized, note_series_labels[curr_series_idx], note_idx_to_interval(last_series_idx, curr_series_idx)))
+                else:
+                    print("#{} Current note: {}".format(num_notes_recognized, note_series_labels[curr_series_idx]))
+
+                num_notes_recognized += 1
             last_series_idx = curr_series_idx
 
         if UPDATE_UI:
